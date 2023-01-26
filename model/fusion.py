e53fcdafd2b6841bc0ebf1ade113b775458d9750
Leejyuan <36397588+Leejyuan@users.noreply.github.com>
2023-01-26 17:43:37 +0800

    Update fusion.py

--- model/fusion.py
+++ model/fusion.py
@@ -7,9 +7,13 @@ Created on Mon Jan 10 16:23:35 2022
 import torch
 import torch.nn as nn
 class SE_Block(nn.Module):
+    '''
+    channel attention block
+    
+    '''
     def __init__(self, ch_in, reduction=16):
         super(SE_Block, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)				# 全局自适应池化
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)				
         self.fc = nn.Sequential(
             nn.Linear(ch_in, ch_in // reduction, bias=False),
             nn.ReLU(inplace=True),
@@ -24,6 +28,11 @@ class SE_Block(nn.Module):
         return  y.expand_as(x)
 
 class DF_fusion_block(nn.Module):
+'''
+AAFM
+x1,x2,x3,x4: features in decoder
+
+'''
     def __init__(self, base_chan):
         super().__init__()
         self.fusion1=nn.Conv2d(15*base_chan, 4*base_chan, kernel_size=1)
@@ -31,10 +40,10 @@ class DF_fusion_block(nn.Module):
                                  nn.BatchNorm2d(4*base_chan),
                                  nn.Sigmoid())
         self.fusion_out=nn.Conv2d(4*base_chan, 4*base_chan, kernel_size = 1)
-        self.atrous1=nn.Conv2d(8*base_chan, base_chan, kernel_size=3,dilation=1, padding=1)
-        self.atrous2=nn.Conv2d(4*base_chan, base_chan, kernel_size=3,dilation=7, padding=7)
-        self.atrous3=nn.Conv2d(2*base_chan, base_chan, kernel_size=3,dilation=13,padding=13)
-        self.atrous4=nn.Conv2d(  base_chan, base_chan, kernel_size=3,dilation=19,padding=19)
+        self.atrous1=nn.Conv2d(8*base_chan, base_chan, kernel_size=3,dilation=2, padding=2)
+        self.atrous2=nn.Conv2d(4*base_chan, base_chan, kernel_size=3,dilation=3, padding=3)
+        self.atrous3=nn.Conv2d(2*base_chan, base_chan, kernel_size=3,dilation=5,padding=5)
+        self.atrous4=nn.Conv2d(  base_chan, base_chan, kernel_size=3,dilation=9,padding=9)
         self.se1=SE_Block(base_chan)
         self.se2=SE_Block(base_chan)
         self.se3=SE_Block(base_chan)
@@ -65,32 +74,19 @@ class DF_fusion_block(nn.Module):
     
 
 
-#import torch
-#import torch.nn as nn
-#class SE_Block(nn.Module):
-#    def __init__(self, ch_in, reduction=4):
-#        super(SE_Block, self).__init__()
-#        self.avg_pool = nn.AdaptiveAvgPool2d(1)				# 全局自适应池化
-#        self.fc = nn.Sequential(
-#            nn.Linear(ch_in, ch_in // reduction, bias=False),
-#            nn.ReLU(inplace=True),
-#            nn.Linear(ch_in // reduction, ch_in, bias=False),
-#            nn.Sigmoid()
-#        )
-#
-#    def forward(self, x):
-#        b, c, _, _ = x.size()
-#        y = self.avg_pool(x).view(b, c)
-#        y = self.fc(y).view(b, c, 1, 1)
-#        return  y.expand_as(x)
-#
+
 #class DF_fusion_block(nn.Module):
+'''
+feature fusion block without adaptive attention correction
+x1,x2,x3,x4: features in decoder
+
+'''
 #    def __init__(self, base_chan):
 #        super().__init__()
-#        self.atrous1=nn.Conv2d(8*base_chan, 8*base_chan, kernel_size=3,dilation=1, padding=1)
-#        self.atrous2=nn.Conv2d(4*base_chan, 4*base_chan, kernel_size=3,dilation=7, padding=7)
-#        self.atrous3=nn.Conv2d(2*base_chan, 2*base_chan, kernel_size=3,dilation=13,padding=13)
-#        self.atrous4=nn.Conv2d(  base_chan,   base_chan, kernel_size=3,dilation=19,padding=19)
+#        self.atrous1=nn.Conv2d(8*base_chan, 8*base_chan, kernel_size=3,dilation=2, padding=2)
+#        self.atrous2=nn.Conv2d(4*base_chan, 4*base_chan, kernel_size=3,dilation=3, padding=3)
+#        self.atrous3=nn.Conv2d(2*base_chan, 2*base_chan, kernel_size=3,dilation=5,padding=5)
+#        self.atrous4=nn.Conv2d(  base_chan,   base_chan, kernel_size=3,dilation=9,padding=9)
 #        self.se1=SE_Block(8*base_chan)
 #        self.se2=SE_Block(4*base_chan)
 #        self.se3=SE_Block(2*base_chan)
